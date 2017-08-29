2464c9b3c30c8b815d9bbeaad30449c6e65c5761
Daniel Majoinen <daniel@majoinen.com>
2017-08-30 02:43:17 +1000

    Remove device related queries

--- src/main/java/com/majoinen/d/pwcrypt/tracker/account/SQLAccountDao.java
+++ src/main/java/com/majoinen/d/pwcrypt/tracker/account/SQLAccountDao.java
@@ -4,6 +4,7 @@ import com.majoinen.d.database.DatabaseController;
 import com.majoinen.d.database.exception.DBUtilsException;
 import com.majoinen.d.encryption.utils.Tools;
 import com.majoinen.d.pwcrypt.tracker.device.Device;
+import com.majoinen.d.pwcrypt.tracker.device.SQLDeviceDao;
 import com.majoinen.d.pwcrypt.tracker.exception.PwCryptException;
 
 import java.util.HashMap;
@@ -17,29 +18,18 @@ import java.util.UUID;
 public class SQLAccountDao implements AccountDao {
 
     // Query to check if an account already exists in the database
-    private static final String ACCOUNT_EXISTS_QUERY =
+    public static final String ACCOUNT_EXISTS_QUERY =
       "SELECT count(*) AS count FROM account WHERE email = :email";
 
     // Query to create a new account
-    static final String CREATE_ACCOUNT_QUERY =
+    public static final String CREATE_ACCOUNT_QUERY =
       "INSERT INTO account (account_uuid, email) " +
         "VALUES (:account_uuid, :email)";
 
-    // Query to insert new device information
-    private static final String CREATE_DEVICE_QUERY =
-      "INSERT INTO device (device_uuid, account_uuid, ip_address, " +
-        "platform, public_key) VALUES (:device_uuid, :account_uuid, " +
-        ":ip_address, :platform, :public_key)";
-
     // Query to insert account verification code
-    private static final String INSERT_ACCOUNT_VERIFY_CODE =
+    public static final String INSERT_ACCOUNT_VERIFY_CODE =
       "INSERT INTO account_verify_code (account_uuid, verify_code) " +
         "VALUES (:account_uuid, :verify_code)";
-
-    // Query to insert device verification code
-    private static final String INSERT_DEVICE_VERIFY_CODE =
-      "INSERT INTO device_verify_code (device_uuid, verify_code) " +
-        "VALUES (:device_uuid, :verify_code)";
 
     private static final int VERIFICATION_CODES_SIZE = 2;
 
@@ -90,8 +80,7 @@ public class SQLAccountDao implements AccountDao {
             return 0 < databaseController
               .prepareQuery(ACCOUNT_EXISTS_QUERY)
               .setParameter(":email", email)
-              .executeAndMap(resultSet ->
-                resultSet.getInt("count"));
+              .executeAndMap(resultSet -> resultSet.getInt("count"));
         } catch(DBUtilsException e) {
             throw new PwCryptException("Error checking if account exists", e);
         }
@@ -125,13 +114,13 @@ public class SQLAccountDao implements AccountDao {
               .prepareBatchQuery(INSERT_ACCOUNT_VERIFY_CODE)
               .setParameter(":account_uuid", accountUUID)
               .setParameter(":verify_code", verifyCodes[ACCOUNT_CODE_INDEX])
-              .prepareBatchQuery(CREATE_DEVICE_QUERY)
+              .prepareBatchQuery(SQLDeviceDao.CREATE_DEVICE_QUERY)
               .setParameter(":device_uuid", device.getUuid())
               .setParameter(":account_uuid", accountUUID)
               .setParameter(":ip_address", device.getIp())
               .setParameter(":platform", device.getPlatform())
               .setParameter(":public_key", device.getPublicKey())
-              .prepareBatchQuery(INSERT_DEVICE_VERIFY_CODE)
+              .prepareBatchQuery(SQLDeviceDao.INSERT_DEVICE_VERIFY_CODE)
               .setParameter(":device_uuid", device.getUuid())
               .setParameter(":verify_code", verifyCodes[DEVICE_CODE_INDEX])
               .executeUpdate()) {
