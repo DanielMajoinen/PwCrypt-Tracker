1d4e09ef7c5a6d1570771b2a6ee38a80b3088c1a
Daniel Majoinen <daniel@majoinen.com>
2017-08-29 22:46:14 +1000

    Add all route paths

---
+++ src/main/java/com/majoinen/d/pwcrypt/tracker/util/Path.java
@@ -0,0 +1,31 @@
+package com.majoinen.d.pwcrypt.tracker.util;
+
+/**
+ * Path namespace.
+ *
+ * @author Daniel Majoinen
+ * @version 1.0, 5/7/17
+ */
+public class Path {
+    public static class Web {
+        // Route for registering an account
+        public static final String REGISTER =
+          "/register/:email/:device_uuid/:public_key/";
+
+        // Route for verify email address and device
+        public static final String VERIFY_EMAIL_DEVICE =
+          "/verify-email-device/:email_code/:device_code/";
+
+        // Route for logging in on a new device with existing account
+        public static final String LOGIN =
+          "/login/:email/:device_uuid/:public_key/";
+
+        // Route for verifying device after login
+        public static final String VERIFY_DEVICE =
+          "/verify-device/:device_code/";
+
+        // Route for listing all devices associated with account
+        public static final String LIST_ALL_DEVICES =
+          "/list/:account_uuid/:device_uuid/";
+    }
+}
