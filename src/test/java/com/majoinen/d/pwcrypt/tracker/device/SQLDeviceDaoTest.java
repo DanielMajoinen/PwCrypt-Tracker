9a130e9c4d658f08962f9fdfe1d0394430d15e52
Daniel Majoinen <daniel@majoinen.com>
2017-08-30 02:50:04 +1000

    Add existing account and device to database for tests

--- src/test/java/com/majoinen/d/pwcrypt/tracker/device/SQLDeviceDaoTest.java
+++ src/test/java/com/majoinen/d/pwcrypt/tracker/device/SQLDeviceDaoTest.java
@@ -2,6 +2,7 @@ package com.majoinen.d.pwcrypt.tracker.device;
 
 import com.majoinen.d.database.DatabaseController;
 import com.majoinen.d.pwcrypt.tracker.TestDatabaseManager;
+import com.majoinen.d.pwcrypt.tracker.account.SQLAccountDao;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,17 +20,25 @@ public class SQLDeviceDaoTest {
     private static final String EXISTING_ACC_UUID =
       "92a37290-728a-49f2-9589-57378acb3adc";
 
+    private static final String EXISTING_EMAIL = "existing@domain.com";
+
     private static final String EXISTING_DEV_UUID =
       "d1ae9719-6001-486c-bc10-2b89b43f26ee";
+
+    private static final String EXISTING_DEV_IP = "192.168.0.1";
+
+    private static final String EXISTING_DEV_PLATFORM = "Desktop";
+
+    private static final String EXISTING_DEV_PUBLIC_KEY = "pubkey1";
 
     private static final String NEW_DEV_UUID =
       "f0658a55-660d-4f53-979c-411e75271ed0";
 
-    private static final String NEW_DEV_IP = "192.168.0.1";
+    private static final String NEW_DEV_IP = "192.168.0.2";
 
     private static final String NEW_DEV_PLATFORM = "Desktop";
 
-    private static final String NEW_DEV_PUBLIC_KEY = "pubkey";
+    private static final String NEW_DEV_PUBLIC_KEY = "pubkey2";
 
     private static final Device NEW_DEVICE = new Device(NEW_DEV_UUID,
       NEW_DEV_IP, NEW_DEV_PLATFORM, NEW_DEV_PUBLIC_KEY);
@@ -41,6 +50,19 @@ public class SQLDeviceDaoTest {
     public void beforeEachTest() throws Exception {
         databaseController = TestDatabaseManager.initDatabaseController();
         deviceDao = SQLDeviceDao.getInstance(databaseController);
+
+        databaseController.prepareQuery(SQLAccountDao.CREATE_ACCOUNT_QUERY)
+          .setParameter(":account_uuid", EXISTING_ACC_UUID)
+          .setParameter(":email", EXISTING_EMAIL)
+          .executeUpdate();
+
+        databaseController.prepareQuery(SQLDeviceDao.CREATE_DEVICE_QUERY)
+          .setParameter(":device_uuid", EXISTING_DEV_UUID)
+          .setParameter(":account_uuid", EXISTING_ACC_UUID)
+          .setParameter(":ip_address", EXISTING_DEV_IP)
+          .setParameter(":platform", EXISTING_DEV_PLATFORM)
+          .setParameter(":public_key", EXISTING_DEV_PUBLIC_KEY)
+          .executeUpdate();
     }
 
     @After
