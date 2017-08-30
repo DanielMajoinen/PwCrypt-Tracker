926f6a33b2ae953bd1fa2709d7d959135242b32b
Daniel Majoinen <daniel@majoinen.com>
2017-08-31 01:32:58 +1000

    Add verify skeleton route

--- src/main/java/com/majoinen/d/pwcrypt/tracker/device/DeviceController.java
+++ src/main/java/com/majoinen/d/pwcrypt/tracker/device/DeviceController.java
@@ -1,10 +1,26 @@
 package com.majoinen.d.pwcrypt.tracker.device;
 
+import com.google.gson.Gson;
+import com.majoinen.d.pwcrypt.tracker.account.AccountController;
+import com.majoinen.d.pwcrypt.tracker.log.LogManager;
+import com.majoinen.d.pwcrypt.tracker.log.Logger;
+import com.majoinen.d.pwcrypt.tracker.util.Path;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.post;
+
 /**
  * @author Daniel Majoinen
  * @version 1.0, 5/7/17
  */
 public class DeviceController {
+
+    private static final Logger LOGGER =
+      LogManager.getLogger(DeviceController.class);
+
+    private static final Gson GSON = new Gson();
 
     private SQLDeviceDao deviceDao;
 
@@ -13,6 +29,10 @@ public class DeviceController {
     }
 
     public void createRoutes() {
+        post(Path.Web.VERIFY, verify, GSON::toJson);
+    }
 
+    private Route verify = (Request request, Response response) -> {
+        return null;
     }
 }
