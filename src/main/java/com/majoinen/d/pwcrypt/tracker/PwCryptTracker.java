68e22c3be85a24497ce0bd676e65871286632692
Daniel Majoinen <daniel@majoinen.com>
2017-08-30 16:35:54 +1000

    Remove refactored spark methods

--- src/main/java/com/majoinen/d/pwcrypt/tracker/PwCryptTracker.java
+++ src/main/java/com/majoinen/d/pwcrypt/tracker/PwCryptTracker.java
@@ -2,40 +2,33 @@ package com.majoinen.d.pwcrypt.tracker;
 
 import com.majoinen.d.database.DatabaseController;
 import com.majoinen.d.database.DatabaseControllerFactory;
-import com.majoinen.d.database.exception.DBUtilsException;
 import com.majoinen.d.pwcrypt.tracker.account.AccountController;
 import com.majoinen.d.pwcrypt.tracker.account.SQLAccountDao;
-import com.majoinen.d.pwcrypt.tracker.util.Filters;
-import com.majoinen.d.pwcrypt.tracker.util.Path;
-
-import static spark.Spark.*;
+import com.majoinen.d.pwcrypt.tracker.device.DeviceController;
+import com.majoinen.d.pwcrypt.tracker.device.SQLDeviceDao;
+import com.majoinen.d.pwcrypt.tracker.spark.SparkManager;
 
 /**
- * PwCryptTracker simply keeps track of accounts device IP addresses when using
- * auto peer discovery in PwCrypt.
- *
  * @author Daniel Majoinen
  * @version 1.0, 5/7/17
  */
 public class PwCryptTracker {
-    public static void main(String[] args) throws DBUtilsException {
+
+    private static final String DB_NAME = "pwcrypt-tracker";
+
+    public static void main(String[] args) throws Exception {
+        // Initialise spark
+        SparkManager.initSpark();
+        // Instantiate controllers
         DatabaseController databaseController =
-          DatabaseControllerFactory.getController("pwcrypt-tracker");
+          DatabaseControllerFactory.getController(DB_NAME);
+        AccountController accountController = new AccountController(
+          SQLAccountDao.getInstance(databaseController));
+        DeviceController deviceController = new DeviceController(
+          SQLDeviceDao.getInstance(databaseController));
+        // Initialise database and create routes
         databaseController.init();
-
-        AccountController accountController =
-          new AccountController(SQLAccountDao.getInstance(databaseController));
-
-        // Configure Spark
-        port(4567);
-
-        // Set up before-filters
-        before("*", Filters.addTrailingSlashes);
-
-        // Add routes
-        get(Path.Web.REGISTER, accountController.register);
-
-        // Set up after-filters
-        after("*", Filters.addGzipHeader);
+        accountController.createRoutes();
+        deviceController.createRoutes();
     }
 }
