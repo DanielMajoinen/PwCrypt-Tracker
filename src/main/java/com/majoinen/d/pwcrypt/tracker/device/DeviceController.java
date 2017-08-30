7ac0cfb339a419fc56e7d9276624e2efa8237ee1
Daniel Majoinen <daniel@majoinen.com>
2017-08-31 03:21:27 +1000

    Implement verify

--- src/main/java/com/majoinen/d/pwcrypt/tracker/device/DeviceController.java
+++ src/main/java/com/majoinen/d/pwcrypt/tracker/device/DeviceController.java
@@ -1,14 +1,21 @@
 package com.majoinen.d.pwcrypt.tracker.device;
 
 import com.google.gson.Gson;
-import com.majoinen.d.pwcrypt.tracker.account.AccountController;
+import com.majoinen.d.encryption.exception.EncryptionUtilsException;
+import com.majoinen.d.encryption.pkc.PKCUtils;
+import com.majoinen.d.pwcrypt.tracker.account.SQLAccountDao;
 import com.majoinen.d.pwcrypt.tracker.log.LogManager;
 import com.majoinen.d.pwcrypt.tracker.log.Logger;
+import com.majoinen.d.pwcrypt.tracker.spark.ResponseMessage;
+import com.majoinen.d.pwcrypt.tracker.spark.SignedJSON;
 import com.majoinen.d.pwcrypt.tracker.util.Path;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.security.PublicKey;
+
+import static com.majoinen.d.pwcrypt.tracker.spark.SparkManager.error400;
 import static spark.Spark.post;
 
 /**
@@ -20,19 +27,92 @@ public class DeviceController {
     private static final Logger LOGGER =
       LogManager.getLogger(DeviceController.class);
 
+    private static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
+
     private static final Gson GSON = new Gson();
 
     private SQLDeviceDao deviceDao;
+    private SQLAccountDao accountDao;
 
-    public DeviceController(SQLDeviceDao deviceDao) {
+    public DeviceController(SQLDeviceDao deviceDao, SQLAccountDao accountDao) {
         this.deviceDao = deviceDao;
+        this.accountDao = accountDao;
     }
 
     public void createRoutes() {
         post(Path.Web.VERIFY, verify, GSON::toJson);
+        post(Path.Web.LOGIN, login, GSON::toJson);
     }
 
+    /**
+     * Attempt to verify an account with the following steps:
+     * 1. Verify signature
+     * 2. Attempt to verify device
+     */
     private Route verify = (Request request, Response response) -> {
-        return null;
+        LOGGER.debug("Received verify request from: " + request.ip());
+        SignedJSON signedJSON = GSON.fromJson(request.body(), SignedJSON.class);
+        DeviceVerificationRequest verificationRequest = GSON.fromJson(signedJSON
+          .getOriginal(), DeviceVerificationRequest.class);
+        // Get variables from request
+        String email = verificationRequest.getEmail();
+        String deviceUUID = verificationRequest.getDeviceUUID();
+        String accountUUID = accountDao.getAccountUUID(email);
+        if(accountUUID == null)
+            return error400(response, "Account error");
+        // Verify signature
+        LOGGER.debug("Verifying signature");
+        if(!verifySignedVerificationRequest(accountUUID, deviceUUID, signedJSON))
+            return error400(response, "Verifying signature failed");
+        // Attempt to verify device
+        LOGGER.debug("Attempting to verify device");
+        if(!deviceDao.verifyDevice(verificationRequest.getCode()))
+            return error400(response, "Failed to verify device");
+        // Successfully verified device
+        return new ResponseMessage("Successfully verified");
+    };
+
+    /**
+     * Verify a signed device verification request.
+     *
+     * @param accountUUID The account UUID of the user.
+     * @param deviceUUID The UUID of the users device.
+     * @param signedJSON SignedJSON received from user.
+     * @return True if the verification is successful, or false otherwise.
+     */
+    private boolean verifySignedVerificationRequest(String accountUUID, String
+      deviceUUID, SignedJSON signedJSON) {
+        try {
+            PublicKey publicKey = getUsersPublicKey(accountUUID, deviceUUID);
+            if(publicKey == null)
+                return false;
+            // Verify signature
+            return PKCUtils.verifyBase64Signature(SIGNATURE_ALGORITHM,
+              publicKey, signedJSON.getSignature(), signedJSON.getOriginal());
+        } catch(EncryptionUtilsException e) {
+            LOGGER.error("Error verifying signature of " +
+              "DeviceVerificationRequest", e);
+            return false;
+        }
+    }
+
+    /**
+     * Get a specified users public key, used when verifying a signed request.
+     *
+     * @param accountUUID The account UUID of the user.
+     * @param deviceUUID The UUID of the device the user used to send the
+     * request.
+     * @return The users device specific public key.
+     */
+    private PublicKey getUsersPublicKey(String accountUUID, String deviceUUID) {
+        String encodedKey = deviceDao.getPublicKey(accountUUID, deviceUUID);
+        if(accountUUID == null || encodedKey == null)
+            return null;
+        try {
+            return PKCUtils.deserializeRSAPublicKey(encodedKey);
+        } catch(EncryptionUtilsException e) {
+            LOGGER.error("Error getting public key", e);
+            return null;
+        }
     }
 }
